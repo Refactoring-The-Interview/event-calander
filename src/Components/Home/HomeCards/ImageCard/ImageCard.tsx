350ae4fcfc6dce2dd39f06108b7b2562e5a73102
sbrandon0432 <sbrandon0432@yahoo.com>
2023-03-28 15:59:17 -0400

    image card and carosel

--- src/Components/Home/HomeCards/ImageCard/ImageCard.tsx
+++ src/Components/Home/HomeCards/ImageCard/ImageCard.tsx
@@ -1,11 +1,15 @@
 import { Card } from "react-bootstrap";
 import { ImageCarousel } from "../../../ImageCarousel/ImageCarousel";
+import InsideStore from "../../../../Assets/Images/cropped-store-image.png";
+import StoreFront from "../../../../Assets/Images/storeFront-lvlG.jpeg";
+import GameWall from "../../../../Assets/Images/gameWall-lvlG.jpeg";
 
 export const ImageCard = () => {
+    const images = [StoreFront, InsideStore, GameWall];
     return (
         <Card>
             <Card.Body>
-                <ImageCarousel />
+                <ImageCarousel images={images} />
             </Card.Body>
         </Card>
     );
--- src/Components/ImageCarousel/ImageCarousel.tsx
+++ src/Components/ImageCarousel/ImageCarousel.tsx
@@ -1,40 +1,23 @@
 import { Carousel } from "react-bootstrap";
-import image from "../../Assets/Images/cropped-store-image.png";
 
-export const ImageCarousel = () => {
+interface Props {
+    images: string[];
+}
+
+export const ImageCarousel = ({ images }: Props) => {
     return (
         <Carousel fade>
-            <Carousel.Item>
-                <img className="d-block w-100" src={image} alt="First slide" />
-                <Carousel.Caption>
-                    <h3>First slide label</h3>
-                    <p>
-                        Nulla vitae elit libero, a pharetra augue mollis
-                        interdum.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className="d-block w-100" src={image} alt="Second slide" />
-
-                <Carousel.Caption>
-                    <h3>Second slide label</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img className="d-block w-100" src={image} alt="Third slide" />
-
-                <Carousel.Caption>
-                    <h3>Third slide label</h3>
-                    <p>
-                        Praesent commodo cursus magna, vel scelerisque nisl
-                        consectetur.
-                    </p>
-                </Carousel.Caption>
-            </Carousel.Item>
+            {images.map((img, index) => {
+                return (
+                    <Carousel.Item>
+                        <img
+                            className="d-block w-100"
+                            src={img}
+                            alt={`sidle ${index}`}
+                        />
+                    </Carousel.Item>
+                );
+            })}
         </Carousel>
     );
 };
