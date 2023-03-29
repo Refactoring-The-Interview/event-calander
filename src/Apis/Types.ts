e8888a976c010b66bbab4e9747e07052eb6c46cd
sbrandon0432 <sbrandon0432@yahoo.com>
2023-03-29 16:21:23 -0400

    dummy data generated and card dates created

--- src/Apis/Types.ts
+++ src/Apis/Types.ts
@@ -1,3 +1,19 @@
 export enum Paths {
     HOME = "/HOME",
+    EVENTCALENDER = "/EVENTCALENDER",
+}
+
+export interface EventDay {
+    date: number;
+    day: string;
+    events: Event[];
+}
+
+export interface Event {
+    eventName: string;
+    joined: string[];
+    description: string;
+    location: string;
+    eventLeader: string;
+    otherInfo: any;
 }
