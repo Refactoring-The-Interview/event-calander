f9aae8d451aabbc7ce078fa98657af6f429cc948
sbrandon0432 <sbrandon0432@yahoo.com>
2023-03-29 17:17:19 -0400

    calander days basic

--- src/Components/DayCard/DayCard.tsx
+++ src/Components/DayCard/DayCard.tsx
@@ -7,15 +7,25 @@ interface Props {
 }
 
 export const DayCard = ({ event }: Props) => {
-    const { date, day } = event;
+    const { date, day, events } = event;
+
     return (
         <div className="DayCard">
             <Card>
                 <Card.Header>
                     <h3>{date}</h3>
-                    <Button>Info</Button>
                 </Card.Header>
-                <Card.Body></Card.Body>
+                <Card.Body className="CardEvents">
+                    {events.map((event) => {
+                        const { eventName } = event;
+                        return (
+                            <div className="Event">
+                                {eventName}
+                                <Button>Info</Button>
+                            </div>
+                        );
+                    })}
+                </Card.Body>
             </Card>
         </div>
     );
