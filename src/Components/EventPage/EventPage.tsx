4cd5d2f4f039b79326b454c3496d1001532eff33
sbrandon0432 <sbrandon0432@yahoo.com>
2023-03-30 14:59:10 -0400

    event cards files created

--- src/Components/EventPage/EventPage.tsx
+++ src/Components/EventPage/EventPage.tsx
@@ -1,30 +1,27 @@
 import { useContext } from "react";
 import { useParams } from "react-router-dom";
+import { Event, EventDay } from "../../Apis/Types";
 import { MyCalenderContext } from "../Context/CalenderContext";
 
 export const EventPage = () => {
     const { eventId } = useParams();
     const { events } = useContext(MyCalenderContext);
 
-    const day = events.find((day) => {
+    const day: EventDay | undefined = events.find((day) => {
         return day.events.find((event) => {
             return event.eventId === eventId;
         });
     });
-    const event = day?.events.find((event) => {
+    const event: Event | undefined = day?.events.find((event) => {
         return event.eventId === eventId;
     });
 
-    console.log(event);
+    if (!event) {
+        return <>Loading...</>;
+    }
 
-    return (
-        <div>
-            <h3>event</h3>
-            <h3>event</h3>
-            <h3>event</h3>
-            <h3>event</h3>
-            <h3>event</h3>
-            <h3>event</h3>
-        </div>
-    );
+    const { eventName, joined, location, eventLeader, description, otherInfo } =
+        event;
+
+    return <div className="EventPage"></div>;
 };
